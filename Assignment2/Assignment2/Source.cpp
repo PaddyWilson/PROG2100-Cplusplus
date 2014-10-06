c91385e1c24d2180758953f70b518e464b57a29d
w0271351 <w0271351@nscc.ca>
2014-10-05 21:25:27 -0300

    < >, can enter numbers for testing

--- Assignment2/Assignment2/Source.cpp
+++ Assignment2/Assignment2/Source.cpp
@@ -2,6 +2,7 @@
 #include <conio.h>
 #include <iostream>
 #include <stdio.h>
+#include <sstream>
 
 using namespace std;
 
@@ -15,6 +16,11 @@ public:
 	RationalNumber() :numerator(0), denominator(1){}//default
 	RationalNumber(int numerator) :numerator(numerator), denominator(1){}//if one number is entered
 	RationalNumber(int numerator, int denominator) :numerator(numerator), denominator(denominator){}//two numbers entered
+
+	void normalize()
+	{
+
+	}
 
 	//addition
 	RationalNumber operator+ (RationalNumber &rightObj)
@@ -97,17 +103,22 @@ public:
 	//greater than
 	bool operator> (RationalNumber &rightObj)
 	{
-		if ((this->numerator * rightObj.denominator) == (this->denominator * rightObj.numerator))
+		//broken
+		if ((this->numerator * rightObj.denominator) > (this->denominator * rightObj.numerator))
 		{
 			return true;
 		}
 		return false;
 	}
 
-	//less than
+	//less than 
 	bool operator< (RationalNumber &rightObj)
 	{
-		return RationalNumber(this->numerator * rightObj.denominator, this->denominator * rightObj.numerator);
+		if ((this->numerator * rightObj.denominator) < (this->denominator * rightObj.numerator))
+		{
+			return true;
+		}
+		return false;
 	}
 
 	//prints operators
@@ -119,42 +130,109 @@ public:
 	//}
 };
 
-//void operator>> (istream &input, RationalNumber &num)
-//{
-//	string str;
-//	getline(input, str);//assign input to string var
-//	num = RationalNumber(str);//call string constructor for "a+bi"
-//}
+void operator>> (istream &input, RationalNumber &num)
+{
+	string str;
+	getline(input, str);//assign input to string var
+
+
+
+
+	//char string[] = str.str();
+
+
+	//num = RationalNumber(str);//call string constructor for "a+bi"
+}
 
 //clean - output is an output stream in a+bi format
 ostream &operator<< (ostream &output, RationalNumber &num)
 {
-	output << num.numerator << "/" << num.denominator;
+	/*if (num.denominator == 1)
+	{
+		output << num.numerator;
+	}
+	else
+	{*/
+		output << num.numerator << "/" << num.denominator;
+	//}
+	
 	return output;// return outstream object
 }
 
+//strtok example
+void strtokExample()
+{
+	int numT = 0;
+	char str[] = "1/9";
+	char * pch;
+	printf("Splitting string \"%s\" into tokens:\n\n", str);
+	pch = strtok(str, "/");
+	while (pch != NULL)
+	{
+		printf("%s\n", pch);
+		pch = strtok(NULL, " ,.-");
+		numT++;
+	}
+	cout << "\nTotal tolkens: " << numT << endl;
+	cout << "press any key for next example" << endl;
+	cin.get();
+
+}//end method strtokExample()
 
 int main()
 {
-	RationalNumber rational1(5, 4);
-	RationalNumber rational2(1, 6);
-	RationalNumber rational3(1, 6);
-	RationalNumber radd = rational1 + rational2;
-	RationalNumber rsub = rational1 - rational2;
-	RationalNumber rmult = rational1 * rational2;
-	RationalNumber rdiv = rational1 / rational2;
+	string input;
+	int n;
+	int d;
 
-	cout << "First: " << rational1 << endl;
-	cout << "Second: " << rational2 << endl;
-	cout << "Add: " << radd << endl;
-	cout << "Sub: " << rsub << endl;
-	cout << "Mult: " << rmult << endl;
-	cout << "Div: " << rdiv << endl;
+	cout << "number 1" << endl;
+	cout << "numerator" << endl;
+	getline(cin, input);
+	n = atoi(input.c_str());
+	cout << "denom" << endl;
+	getline(cin, input);
+	d = atoi(input.c_str());
 
-	if (rational2 == rational3)
-	{
-		cout << "true" << endl;
-	}
+RationalNumber rational1(n, d);
+
+cout << endl;
+	cout << "number 2" << endl;
+	cout << "numerator" << endl;
+	getline(cin, input);
+	n = atoi(input.c_str());
+	cout << "denom" << endl;
+	getline(cin, input);
+	d = atoi(input.c_str());
+
+	RationalNumber rational2(n, d);
+
+	cout << endl;
+	cout << "First Rational: " << rational1 << endl;
+	cout << "Second Rational: " << rational2 << endl;
+	cout << "Add: " << rational1 + rational2 << endl;
+	cout << "Sub: " << rational1 - rational2 << endl;
+	cout << "Mult: " << rational1 * rational2 << endl;
+	cout << "Div: " << rational1 / rational2 << endl;
+	
+	cout << "Rational1 == Rational2: ";
+	if (rational1 == rational2)
+		cout << "True" << endl;
+	else
+		cout << "False" << endl;
+
+	cout << "Rational1 > Rational2: ";
+	if (rational1 > rational2)
+		cout << "True" << endl;
+	else
+		cout << "False" << endl;
+
+	cout << "Rational1 < Rational2: ";
+	if (rational1 < rational2)
+		cout << "True" << endl;
+	else
+		cout << "False" << endl;
+
+	strtokExample();
 
 	_getch();
 	return 0;
